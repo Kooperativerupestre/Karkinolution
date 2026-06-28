860737136dd5b2e7183fc98c89db98649c7a84ab
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-06-28 13:23:04 -0300

    add automated tests

---
+++ tests/test_death.py
@@ -0,0 +1,24 @@
+from organism.creatures import CreatureFactory
+from core.coord import Coord
+from systems.death_system import DeathSystem
+
+
+def test_verify_creature_death() -> None:
+    # INIT
+
+    creature = CreatureFactory.gen_creature(position=Coord(1, 1))
+    # ENERGY VERIFICATION
+    creature.energy.value = 0
+
+    assert DeathSystem.is_dead(creature) == True, "Creature with energy = 0 must die"
+    creature.energy.value=creature.energy.limit
+    # LIFE VERIFICATION
+
+    creature.life.value = 0
+    assert DeathSystem.is_dead(creature) == True, "Creature with life = must die"
+    creature.life.value=creature.life.limit     
+    # AGE VERIFICATION
+    creature.age.value=creature.age.limit
+    assert DeathSystem.is_dead(creature) == True, "Creature with age = max age must die"
+
+    
