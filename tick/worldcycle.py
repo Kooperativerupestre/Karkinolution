4ce32edbbf8ebb296a4c3b07bdb6cbe2c4316913
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-07-02 16:32:33 -0300

    Fix function inputs and typo names in worldcycle.py

--- tick/worldcycle.py
+++ tick/worldcycle.py
@@ -7,7 +7,7 @@ class Runner:
     @staticmethod
     def run(world:World):
         territory = world.territory
-        entitys = world.entities
+        entities = world.entities
         entity_map = world.entity_map
 
         ### RUN CELLS
@@ -19,11 +19,11 @@ class Runner:
         for coord, id in list(entity_map.iter):
             cell = territory.get(coord)
             if id.e_type == EntityTypes.CREATURE:
-                creature = entitys.get_creature(id)
+                creature = entities.get_creature(id)
 
                 RunnerCreature.run_creature(creature, world)
             elif id.e_type == EntityTypes.CORPSE:
-                corpse = entitys.get_corpse(id)
+                corpse = entities.get_corpse(id)
 
-                RunnerCorpse.run_corpse(corpse, coord, entity_map, entitys)
+                RunnerCorpse.run_corpse(corpse, entity_map, entities)
         world.time+=1
