e204f3b90f6b6239e8d106980f005fcf5e8fedad
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-07-03 17:53:05 -0300

    add more tests in test_reproduction

--- tests/test_reproduction.py
+++ tests/test_reproduction.py
@@ -1,7 +1,7 @@
 from organism.creatures import CreatureFactory, PregnantUterus, EmptyUterus, Gender
-from systems.reproduction import UterusSystem
+from systems.reproduction import UterusSystem, ReproductiveSystem
 from organism.genetics import creatures_genomes, CreatureTypes
-
+from decisions.perception import Perceiver
 def test_uterus_female() -> None:
     female = CreatureFactory.gen_creature(gender=Gender.FEMALE)
 
@@ -17,3 +17,29 @@ def test_uterus_conceive() -> None:
     UterusSystem.conceive(female, creatures_genomes.get_type_genome(CreatureTypes.CRAB))
     assert isinstance(female.uterus, PregnantUterus)
 
+def test_different_species_cannot_reproduce() -> None:
+    female = CreatureFactory.gen_creature(gender=Gender.FEMALE, creature_type=CreatureTypes.CRAB)
+    male = CreatureFactory.gen_creature(gender=Gender.MALE, creature_type=CreatureTypes.HIPPOPOTAMUS)
+
+    assert ReproductiveSystem.can_reproduce(female, Perceiver.perceive_entity(male)) == False
+
+def test_same_gender_cannot_reproduce() -> None:
+    female_1 = CreatureFactory.gen_creature(gender=Gender.FEMALE, creature_type=CreatureTypes.CRAB)
+    female_2 = CreatureFactory.gen_creature(gender=Gender.FEMALE, creature_type=CreatureTypes.CRAB)
+
+    assert ReproductiveSystem.can_reproduce(female_1, Perceiver.perceive_entity(female_2)) == False
+
+def test_opposite_gender_same_species_can_reproduce() -> None:
+    female = CreatureFactory.gen_creature(
+        gender=Gender.FEMALE,
+        creature_type=CreatureTypes.CRAB,
+    )
+    male = CreatureFactory.gen_creature(
+        gender=Gender.MALE,
+        creature_type=CreatureTypes.CRAB,
+    )
+
+    assert ReproductiveSystem.can_reproduce(
+        female,
+        Perceiver.perceive_entity(male),
+    )
