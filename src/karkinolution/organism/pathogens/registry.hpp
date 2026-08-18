807332d2f209a7d4cfac79f267ccaa74ffb5c980
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-08-17 21:07:32 -0300

    refactor: create a separate PathogenRegistry from EntitiesRegistry

---
+++ src/karkinolution/organism/pathogens/registry.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <karkinolution/organism/pathogens/virus/virus.hpp>
+
+using GenericPathogenContainer = std::variant<VirusContainer>;
+
+
+class PathogenRegistry : public BaseStorage<PathogenId, GenericPathogenContainer> {
+    public:
+
+    PathogenRegistry() : BaseStorage<PathogenId, GenericPathogenContainer>() {}
+
+    VirusContainer& at_virus(PathogenId id) {
+        return std::get<VirusContainer>(at(id));
+    }
+    const VirusContainer& at_virus(PathogenId id) const {
+        return std::get<VirusContainer>(at(id));
+    }
+};
