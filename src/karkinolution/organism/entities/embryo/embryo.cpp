4ff1e0acb844dc1fccc7a0f0f1da237719dedd9d
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-08-18 14:09:17 -0300

    enhance: update EmbryoGrowingPhysiology calculations and add species-relative properties to Embryo

--- src/karkinolution/organism/entities/embryo/embryo.cpp
+++ src/karkinolution/organism/entities/embryo/embryo.cpp
@@ -3,3 +3,15 @@
 EmbryoHungry Embryo::hungry() const {
     return (1.0f - life.ratio()) + (1.0f - energy.ratio()) + (1.0f - health.ratio()/2.0f); 
 }
+
+[[nodiscard]] float Embryo::specie_relative_energy_max() const {
+    return energy.max()/genome.calculate_embryo_average_max_energy();
+}
+
+[[nodiscard]] float Embryo::specie_relative_volume() const {
+    return volume.value/genome.calculate_embryo_average_volume().value;
+}
+
+[[nodiscard]] float Embryo::specie_relative_life() const {
+    return life.max()/genome.calculate_embryo_average_life();
+}
