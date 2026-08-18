dc513c8c63a11d0e23f06795f084c23e2dc13eac
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-08-18 14:16:40 -0300

    add: transfer_energy_to_uterus function to UterusMotor

--- src/karkinolution/organism/reproduction/uterus/motor.hpp
+++ src/karkinolution/organism/reproduction/uterus/motor.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "karkinolution/math/stats/compile_values.hpp"
 #include "karkinolution/organism/reproduction/state/state.hpp"
 #include <karkinolution/organism/reproduction/uterus/uterus.hpp>
 #include <karkinolution/organism/reproduction/uterus/physiology.hpp>
@@ -7,4 +8,6 @@
 
 namespace UterusMotor {
     void transfer_energy_to_uterus(Creature&creature);
+    void transfer_energy_to_uterus(Creature&creature, float energy, float reserved);
+    void transfer_energy_to_uterus(Creature&creature, NormalizedValue<float> energy_ratio, NormalizedValue<float> reserved_ratio);
 }
