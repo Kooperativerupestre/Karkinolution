80ab27c0b0d8cb5c89bb7993a80906264ec1da9a
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-08-18 14:40:25 -0300

    add: needed_energy_of_all_embryos to ReproductionOrganPhysiology

--- src/karkinolution/organism/reproduction/state/physiology.hpp
+++ src/karkinolution/organism/reproduction/state/physiology.hpp
@@ -2,8 +2,11 @@
 #include <karkinolution/organism/entities/creature/creature.hpp>
 #include <karkinolution/organism/reproduction/uterus/uterus.hpp>
 #include <karkinolution/organism/reproduction/state/state.hpp>
+#include <karkinolution/organism/registry.hpp>
 
 namespace ReproductionOrganPhysiology {
     int get_children_count(Creature&creature);
-    // not deterministic
+        // not deterministic
+    float needed_energy_of_all_embryos(const ReproductionOrgan&organ, const OrganismRegistry&organisms);
+
 }
