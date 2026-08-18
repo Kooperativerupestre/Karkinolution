6d4e139c7c252b2987bfb11c0a37ddfa0cf56076
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-08-18 14:40:25 -0300

    add: needed_energy_of_all_embryos to ReproductionOrganPhysiology

--- src/karkinolution/organism/reproduction/state/physiology.cpp
+++ src/karkinolution/organism/reproduction/state/physiology.cpp
@@ -1,3 +1,7 @@
+#include "karkinolution/core/id_generator.hpp"
+#include "karkinolution/math/stats/compile_values.hpp"
+#include "karkinolution/organism/entities/genetics/motor.hpp"
+#include "karkinolution/organism/entities/identity.hpp"
 #include "karkinolution/organism/reproduction/state/state.hpp"
 #include <karkinolution/organism/reproduction/state/physiology.hpp>
 #include <karkinolution/utils/k_random.hpp>
@@ -11,3 +15,15 @@ int ReproductionOrganPhysiology::get_children_count(Creature &creature) {
     // Poisson distribution is used here because repeated samples converge to the specified mean.
 }
 
+float ReproductionOrganPhysiology::needed_energy_of_all_embryos(const ReproductionOrgan &organ, const OrganismRegistry &organisms) {
+    ReproductionValidator::is_pregnant(organ);
+
+    float total_energy = 0.0f;
+    const auto& uterus = std::get<Uterus>(organ.state);
+
+    for (auto id : uterus.get_pregnant_uterus().embryos) {
+        const auto& embryo = organisms.entities.at_embryo(IDF::create_embryo_id(id));
+        total_energy += (embryo.energy.max() - embryo.energy.value());
+    }
+    return total_energy;
+}
