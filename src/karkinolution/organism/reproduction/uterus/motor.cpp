4baae6991d181b5ebeb052eedd690433799c4cd9
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-08-18 14:16:40 -0300

    add: transfer_energy_to_uterus function to UterusMotor

--- src/karkinolution/organism/reproduction/uterus/motor.cpp
+++ src/karkinolution/organism/reproduction/uterus/motor.cpp
@@ -1,3 +1,4 @@
+#include "karkinolution/math/stats/compile_values.hpp"
 #include "karkinolution/organism/reproduction/state/state.hpp"
 #include <karkinolution/organism/reproduction/uterus/uterus.hpp>
 #include <karkinolution/organism/reproduction/uterus/physiology.hpp>
@@ -7,22 +8,31 @@
 #include <karkinolution/organism/reproduction/state/validator.hpp>
 
 void UterusMotor::transfer_energy_to_uterus(Creature &creature) {
-    ReproductionValidator::is_pregnant(creature.reproduction);
-    auto uterus = std::get<Uterus>(creature.reproduction.state);
-
-
-    auto uterus_hungry = uterus.hungry();
-
-    float energy_trade = std::min(creature.body.metabolism.energy.value() * 0.2f, uterus_hungry.value() * uterus.energy.max());
-    float reserved_trade = std::min(creature.body.metabolism.reserved.value() * 0.5f, uterus_hungry.value() * uterus.energy.max());
-
-    creature.body.metabolism.energy -= energy_trade;
-    creature.body.metabolism.reserved -= reserved_trade;
-
-    uterus.energy += energy_trade;
-    uterus.energy += reserved_trade;
+    transfer_energy_to_uterus(creature, NormalizedValue<float>(0.2f), NormalizedValue<float>(0.5f));
 }
 
+void UterusMotor::transfer_energy_to_uterus(Creature &creature, float energy, float reserved) {
+    ReproductionValidator::has_uterus(creature.reproduction);
 
+    auto& uterus = std::get<Uterus>(creature.reproduction.state);
 
+    const float uterus_remaining =
+        uterus.energy.max() - uterus.energy.value();
+
+    const float real_energy_needed =
+        std::min(uterus_remaining, energy + reserved);
+
+    const float energy_used =
+        std::min(energy, real_energy_needed);
+
+    const float reserved_used =
+        real_energy_needed - energy_used;
+    uterus.energy += real_energy_needed;
+    creature.body.metabolism.energy -= energy_used;
+    creature.body.metabolism.reserved -= reserved;
+}
+
+void UterusMotor::transfer_energy_to_uterus(Creature&creature, NormalizedValue<float>energy_ratio, NormalizedValue<float> reserved_ratio) {
+    transfer_energy_to_uterus(creature, energy_ratio.value() * creature.body.metabolism.energy.value(), reserved_ratio.value() * creature.body.metabolism.reserved.value());
+}
 
