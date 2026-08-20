74725515f5f101351ef9f55ebe56cedd9e980255
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-08-19 21:03:50 -0300

    add: run corpse

--- src/karkinolution/organism/entities/corpse/motor.cpp
+++ src/karkinolution/organism/entities/corpse/motor.cpp
@@ -1,2 +1,7 @@
 #include <karkinolution/organism/entities/corpse/corpse.hpp>
 #include <karkinolution/organism/registry.hpp>
+#include <karkinolution/organism/entities/corpse/motor.hpp>
+
+void CorpseMotor::run(Corpse &corpse) {
+    corpse.raw_meat.energy *= 0.1;
+}
