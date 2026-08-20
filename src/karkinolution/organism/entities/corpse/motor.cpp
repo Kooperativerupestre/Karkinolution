e78f280304f09ddbcd0bf07bf99514fa2526e420
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-08-19 21:00:20 -0300

    add: CorpseMotor

---
+++ src/karkinolution/organism/entities/corpse/motor.cpp
@@ -0,0 +1,2 @@
+#include <karkinolution/organism/entities/corpse/corpse.hpp>
+#include <karkinolution/organism/registry.hpp>
---
+++ src/karkinolution/organism/entities/corpse/motor.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <karkinolution/organism/registry.hpp>
+#include <karkinolution/organism/entities/corpse/corpse.hpp>
+
+namespace CorpseMotor {
+    void run(Corpse&corpse);
+}
