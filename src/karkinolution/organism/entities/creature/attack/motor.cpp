628fb27481bf85b5f9adeaa0f40395b635e7db2f
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-09-01 14:03:27 -0300

    add(Attack): motor

---
+++ src/karkinolution/organism/entities/creature/attack/motor.cpp
@@ -0,0 +1,14 @@
+
+#include <karkinolution/organism/entities/creature/attack/motor.hpp>
+#include <karkinolution/organism/entities/creature/attack/physiology.hpp>
+#include <karkinolution/organism/entities/creature/creature.hpp>
+#include <karkinolution/organism/stats.hpp>
+
+OrganismStats::Body::Damage AttackMotor::attack(const Creature& attacker, Creature& target) {
+    const Damage damage = AttackPhysiology::calculate_damage(attacker);
+
+    target.body.vital.life -= damage.value;
+
+    target.body.skeleton.quality -= (target.body.vital.life.max() / damage.value) * 0.75f;
+    return damage;
+}
---
+++ src/karkinolution/organism/entities/creature/attack/motor.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#include <karkinolution/organism/entities/creature/attack/physiology.hpp>
+#include <karkinolution/organism/entities/creature/creature.hpp>
+#include <karkinolution/organism/stats.hpp>
+
+namespace AttackMotor {
+OrganismStats::Body::Damage attack(const Creature& attacker, Creature& target);
+}
