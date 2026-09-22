b3c8dfbfef44ff5de66dc93db3f8fa189142bbef
Koöperative Rupestrën <domicuslucinox@gmail.com>
2026-09-22 19:24:55 -0300

    enhance(frame parser): make it more generic and secure

--- src/karkinolution/binary/frames/parser.hpp
+++ src/karkinolution/binary/frames/parser.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstdint>
 #include <deque>
+#include <karkinolution/binary/byte_range.hpp>
+#include <karkinolution/binary/deserialization/deserializer.hpp>
 #include <karkinolution/binary/message_type_size.hpp>
 #include <vector>
 
@@ -12,18 +14,50 @@ struct ParsedFrame {
 };
 
 namespace FrameParser {
-	std::uint32_t get_size(const std::vector<std::byte> &bytes);
-	std::uint32_t get_size(const std::deque<std::byte> &bytes);
+	template <ByteRange T> std::uint32_t get_size(const T &bytes) {
+		return Deserializer::read_uint32_t(bytes, 0);
+	}
 
-	BinaryTypes get_type(const std::vector<std::byte> &bytes);
-	BinaryTypes get_type(const std::deque<std::byte> &bytes);
+	template <ByteRange T> BinaryTypes get_type(const T &bytes) {
+		const auto raw = Deserializer::read_uint8_t(bytes, MESSAGE_SIZE_BYTES);
 
-	BinarySubTypes::CodeSubTypes get_sub_type(const std::vector<std::byte> &bytes);
-	BinarySubTypes::CodeSubTypes get_sub_type(const std::deque<std::byte> &bytes);
+		BinaryValidator::validate_type(raw);
+		return static_cast<BinaryTypes>(raw);
+	}
 
-	std::vector<std::byte> get_payload(const std::deque<std::byte> &bytes);
-	std::vector<std::byte> get_payload(const std::vector<std::byte> &bytes);
+	template <ByteRange T> BinarySubTypes::CodeSubTypes get_sub_type(const T &bytes) {
+		const auto type = get_type(bytes);
+		const auto read =
+			Deserializer::read_uint32_t(bytes, MESSAGE_SIZE_BYTES + MESSAGE_TYPE_BYTES);
 
-	ParsedFrame parse_frame(const std::deque<std::byte> &bytes);
-	ParsedFrame parse_frame(const std::vector<std::byte> &bytes);
+		BinaryValidator::validate_sub_type(type, read);
+
+		if (type == BinaryTypes::Error) {
+
+			return BinarySubTypes::Error{read};
+		} else if (type == BinaryTypes::Request) {
+			return BinarySubTypes::Request{read};
+		} else if (type == BinaryTypes::Response) {
+			return BinarySubTypes::Response{read};
+		}
+		throw ByteError("Unexpected error");
+	}
+
+	template <ByteRange T> std::vector<std::byte> get_payload(const T &bytes) {
+		std::vector<std::byte> payload;
+
+		const auto offset = MESSAGE_SIZE_BYTES + MESSAGE_TYPE_BYTES + MESSAGE_SUB_TYPE_BYTES;
+
+		payload.insert(payload.end(), bytes.begin() + offset, bytes.end());
+
+		return payload;
+	}
+
+	template <ByteRange T> ParsedFrame parse_frame(const T &bytes) {
+		return ParsedFrame{.size     = get_size(bytes),
+						   .type     = get_type(bytes),
+						   .sub_type = get_sub_type(bytes),
+						   .payload  = get_payload(bytes)};
+	}
+
 } // namespace FrameParser
--- src/karkinolution/binary/message_type_size.hpp
+++ src/karkinolution/binary/message_type_size.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <cstdint>
-#include <numeric>
+#include <format>
+#include <karkinolution/core/error.hpp>
 #include <variant>
 /*
 
@@ -47,3 +48,43 @@ namespace BinarySubTypes {
 
 static_assert(sizeof(double) == 8);
 static_assert(std::numeric_limits<double>::is_iec559);
+
+namespace BinaryValidator {
+	constexpr void validate_type(uint8_t value) {
+		if (value != static_cast<uint8_t>(BinaryTypes::Request)
+			&& value != static_cast<uint8_t>(BinaryTypes::Response)
+			&& value != static_cast<uint8_t>(BinaryTypes::Error)) {
+			throw ByteError(std::format("Invalid frame type: {}", value));
+		}
+	}
+
+	constexpr void validate_error_sub_type(uint32_t value) {
+		if (value != static_cast<uint32_t>(BinarySubTypes::Error::CREATURE_WAS_NOT_FOUND)) {
+			throw ByteError(std::format("Invalid frame sub type: {}", value));
+		}
+	}
+
+	constexpr void validate_request_sub_type(uint32_t value) {
+		if (value != static_cast<uint32_t>(BinarySubTypes::Request::GET_CREATURE)) {
+			throw ByteError(std::format("Invalid frame sub type: {}", value));
+		}
+	}
+
+	constexpr void validate_response_sub_type(uint32_t value) {
+		if (value != static_cast<uint32_t>(BinarySubTypes::Response::CREATURE)) {
+			throw ByteError(std::format("Invalid frame sub type: {}", value));
+		}
+	}
+
+	constexpr void validate_sub_type(BinaryTypes type, uint32_t value) {
+		if (type == BinaryTypes::Request) {
+			validate_request_sub_type(value);
+		} else if (type == BinaryTypes::Response) {
+			validate_response_sub_type(value);
+		} else if (type == BinaryTypes::Error) {
+			validate_error_sub_type(value);
+		}
+
+		throw ByteError(std::format("Invalid type: {}", static_cast<uint8_t>(type)));
+	}
+} // namespace BinaryValidator
